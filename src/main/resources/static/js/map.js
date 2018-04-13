28d02e76276b29e6f23f89576c0a0b5ea5f4d763
Denis Trofimov <dtrofimov@diasoft.ru>
2018-04-13 14:20:07 +0300

    привязка фронта к бэку

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    console.log("ready!");
-
 
     var screenHeight = document.documentElement.clientHeight - document.getElementById("footerid").clientHeight - document.getElementById("myNavbar").clientHeight + "px";
     document.getElementById("mapid").style.height = screenHeight;
@@ -188,5 +186,30 @@ $(document).ready(function () {
             }
         }
     });
+    $("#jstree_demo_div").jstree();
 
+    $('#jstree_demo_div').on("changed.jstree", function (e, data) {
+        var sel = data.node.text;
+        switch (sel) {
+            case "Пермь":
+                mymap.setView([58.0099, 56.25], 13);
+                break;
+            case "Березники":
+                mymap.setView([59.41, 56.78], 13);
+                break;
+            case "Чусовой":
+                mymap.setView([58.27, 57.83], 13);
+                break;
+            case "test1":
+                mymap.setView([58.012724, 56.181788], 16);
+                break;
+            case "test2":
+                mymap.setView([58.003221, 56.206545], 16);
+                break;
+            case "test3":
+                mymap.setView([58.007661, 56.186351], 16);
+                break;
+            default:
+        }
+    });
 });
