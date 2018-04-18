43a7db32c895b8d2524a1ccf61d2ab5d571289d6
svnikitin <svnikitin@diasoft.ru>
2018-04-18 10:30:43 +0500

    Изменил справочник Адреса

--- src/main/java/com/example/persistance/entity/Address.java
+++ src/main/java/com/example/persistance/entity/Address.java
@@ -35,16 +35,12 @@ public class Address implements Serializable {
 
     @Id
     @SequenceGenerator(name = "pk_sequence", sequenceName = "address_id_seq", allocationSize = 1)
-    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pk_sequence")
     @Column(name = "id", unique = true, nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne(fetch=FetchType.LAZY, cascade={CascadeType.PERSIST, CascadeType.MERGE})
     private Address parent;
-
-    //@OneToMany(mappedBy = "parent", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    //private Set<Address> children = new HashSet<>();
 
     @Column
     private String name;
@@ -54,19 +50,5 @@ public class Address implements Serializable {
 
     @Column
     private Float longitude;
-
-    /*
-    public Set<Address> collectLeafChildren() {
-        Set<Address> results = new HashSet<>();
-        if (children.isEmpty()) {
-            results.add(this);
-        } else {
-            children.forEach(child -> {
-                results.addAll(child.collectLeafChildren());
-            });
-        }
-        return results;
-    }
-    */
-
+    
 }
