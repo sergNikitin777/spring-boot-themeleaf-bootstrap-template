51d26fb4bcbfe75b00a9975b218eab074fe23382
Никита Гребнев <ngrebnev@diasoft.ru>
2018-04-24 12:04:44 +0500

    апгрейд заглушки

--- src/main/resources/static/js/journal.js
+++ src/main/resources/static/js/journal.js
@@ -8,6 +8,14 @@ $(document).ready(function () {
         $('.navbar-toggle').click()
     });
     $("#tableId tr").click(function() {
-        confirm("Отправить СМС клиенту " + $(this).children(".name").html() + "?");
+        var confirmed = confirm("Отправить СМС клиенту " + $(this).children(".name").html() + "?")
+        if(confirmed) {
+            $(this).removeClass('danger');
+            $(this).addClass('success');
+        }
+        else {
+            $(this).removeClass('success');
+            $(this).addClass('danger');
+        }
     });
 });
