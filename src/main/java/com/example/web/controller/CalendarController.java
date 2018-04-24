e64a100c15c9c135d31be7aa701f9d84c754df1c
svnikitin <svnikitin@diasoft.ru>
2018-04-24 12:27:42 +0500

    Добавлен пост контроллер для получения списка событий из календаря

--- src/main/java/com/example/web/controller/CalendarController.java
+++ src/main/java/com/example/web/controller/CalendarController.java
@@ -1,5 +1,6 @@
 package com.example.web.controller;
 
+import com.example.web.pojo.CalendarReqPojo;
 import com.example.web.service.CalendarService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,10 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -20,7 +23,6 @@ import java.util.List;
 public class CalendarController {
 
     private final CalendarService calendarService;
-
 
     @RequestMapping(value = "/calendar/events", method = RequestMethod.GET)
     public ResponseEntity<List<VEvent>> calendarEvents() {
@@ -36,4 +38,19 @@ public class CalendarController {
         List<VEvent> vEvents = calendarService.findAllEvents(caldavHost, caldavPort, protocol, userName, password, calPrefix, calPostfix);
         return new ResponseEntity<>(vEvents, HttpStatus.OK);
     }
+
+
+    @RequestMapping(value = "/calendar/eventsbyparam", method = RequestMethod.POST)
+    public ResponseEntity<List<VEvent>> calendarEventsByParam(@Valid @RequestBody CalendarReqPojo calendarReqPojo) {
+
+        List<VEvent> vEvents = calendarService.findAllEvents(calendarReqPojo.getCaldavHost(),
+                calendarReqPojo.getCaldavPort(),
+                calendarReqPojo.getProtocol(),
+                calendarReqPojo.getUserName(),
+                calendarReqPojo.getPassword(),
+                calendarReqPojo.getCalPrefix(),
+                calendarReqPojo.getCalPostfix());
+        return new ResponseEntity<>(vEvents, HttpStatus.OK);
+    }
+
 }
