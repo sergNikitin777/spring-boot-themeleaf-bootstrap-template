6cdb330ee5d9af9ba56961007c7a2f6d337e4342
Никита Гребнев <ngrebnev@diasoft.ru>
2018-05-07 04:28:32 +0500

    Добавил визард

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -185,4 +185,36 @@ $(document).ready(function () {
         $('#jstree_demo_div').jstree("select_node", $('#'+searchedNodeId));
     });
 
+    $('.nav-tabs > li a[title]').tooltip();
+
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        var $target = $(e.target);
+
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
 });
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
