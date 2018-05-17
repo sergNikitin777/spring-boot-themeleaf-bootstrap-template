1a0050630711e2bfcd40a5b687553f75c6d0ee53
Denis Trofimov <dtrofimov@diasoft.ru>
2018-05-17 13:01:28 +0300

    Рабочий стол, 3 варианта

--- src/main/resources/static/js/custom-scripts.js
+++ src/main/resources/static/js/custom-scripts.js
@@ -2,4 +2,33 @@ $(document).ready(function () {
     $('[data-toggle=offcanvas]').click(function () {
         $('.row-offcanvas').toggleClass('active');
     });
+
+    $( ".column" ).sortable({
+        connectWith: ".column",
+        handle: ".portlet-header",
+        cancel: ".portlet-toggle",
+        placeholder: "portlet-placeholder ui-corner-all"
+    });
+
+    $( ".portlet" )
+        .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+        .find( ".portlet-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+    $( ".portlet-toggle" ).on( "click", function() {
+        var icon = $( this );
+        icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+        icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    });
+
+    //Определяем рабочую область для фрейма Рабочий стол
+    var footerTop = $('#footerid').position().top;
+    var monitor1Top = $('#monitor1').position().top;
+    var footerHeight = $('#footerid').height();
+    $('#monitor1').css('height', footerTop - monitor1Top - footerHeight);
+
+
+    $(".dragman").draggable();
+    $(".dragman").resizable();
 });
