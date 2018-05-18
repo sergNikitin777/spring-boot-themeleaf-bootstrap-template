94ee2f100690efb17e6f93241e03b4a13f1c09e7
Denis Trofimov <dtrofimov@diasoft.ru>
2018-05-18 14:55:09 +0300

    Табличка в разделе Задачи

--- src/main/resources/static/js/custom-scripts.js
+++ src/main/resources/static/js/custom-scripts.js
@@ -31,4 +31,41 @@ $(document).ready(function () {
 
     $(".dragman").draggable();
     $(".dragman").resizable();
+
+
+    //Грид
+    var dataArray = [
+        {number: '1', ready: 'В работе', description: 'ТО-1', status: '5', start: '01.01.2001', deadline: '02.03.2001', equipment: 'СРУ БАО-600', address: 'г.Пермь, ул. Ленина, 88',customer: 'Иванов И.И.',performer: 'Сидоров А.А.'},
+        {number: '2', ready: 'Ожидается', description: 'ТО-2', status: '3', start: '01.01.2002', deadline: '02.03.2002', equipment: 'СРУ БАО-600', address: 'г.Пермь, ул. Ленина, 88',customer: 'Иванов И.И.',performer: 'Сидоров А.А.'},
+        {number: '3', ready: 'Готово', description: 'ТО-1', status: '5', start: '01.01.2001', deadline: '02.03.2001', equipment: 'СРУ БАО-600', address: 'г.Пермь, ул. Ленина, 88',customer: 'Иванов И.И.',performer: 'Сидоров А.А.'},
+        {number: '4', ready: 'В работе', description: 'ТО-3', status: '5', start: '01.01.2001', deadline: '02.03.2001', equipment: 'СРУ БАО-600', address: 'г.Пермь, ул. Ленина, 88',customer: 'Иванов И.И.',performer: 'Сидоров А.А.'},
+        {number: '5', ready: 'В работе', description: 'ТО-1', status: '5', start: '01.01.2001', deadline: '02.03.2001', equipment: 'СРУ БАО-600', address: 'г.Пермь, ул. Ленина, 88',customer: 'Иванов И.И.',performer: 'Сидоров А.А.'}
+    ];
+
+    $("#jqGrid").jqGrid({
+        datatype: 'local',
+        data: dataArray,
+        colModel: [
+            {name: 'number', label : '№', align: 'center'},
+            {name: 'ready', label : 'Готовность', align: 'center'},
+            {name: 'description', label : 'Описание', align: 'center'},
+            {name: 'status', label : 'Статус', align: 'center'},
+            {name: 'start', label : 'Начало', align: 'center'},
+            {name: 'deadline', label : 'Крайний срок', align: 'center'},
+            {name: 'equipment', label : 'Оборудование', align: 'center'},
+            {name: 'address', label : 'Адрес', align: 'center'},
+            {name: 'customer', label : 'Заказчик', align: 'center'},
+            {name: 'performer', label : 'Исполнитель', align: 'center'}
+        ],
+        caption: 'Список задач',
+        height: 'auto',
+        rowNum: 5,
+        // styleUI : 'Bootstrap',
+        pager: '#pager',
+        align: 'center',
+        autowidth: true
+
+    });
+
+
 });
