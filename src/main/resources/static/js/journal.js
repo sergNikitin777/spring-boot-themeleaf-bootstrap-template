28ef3d35237f9741bfcd76b3d554d236f4e12bc9
Никита Гребнев <ngrebnev@diasoft.ru>
2018-05-21 17:24:35 +0500

    логин-заглушка v0.1

--- src/main/resources/static/js/journal.js
+++ src/main/resources/static/js/journal.js
@@ -394,6 +394,43 @@ $(document).ready(function () {
         });
     });
 
+    var slideCount = $('#slider ul li').length;
+    var slideWidth = $('#slider ul li').width();
+    var slideHeight = $('#slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+
+    $('#slider').css({ width: slideWidth, height: slideHeight });
+
+    $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+
     // Пользовательские события
     $(document).on('click', '.row-header' , function(){
         $(this).toggleClass('expand').nextUntil('tr.row-header').slideToggle(100);
