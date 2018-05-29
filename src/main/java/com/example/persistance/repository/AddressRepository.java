087ae34c445823024f76afe3b7cd5baa940918ab
svnikitin <svnikitin@diasoft.ru>
2018-05-29 15:39:26 +0500

    remove trash

--- src/main/java/com/example/persistance/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.persistance.repository;
-
-import com.example.persistance.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Integer>
-{
-
-}
