d603fe46c5c1e17d02bd78774fc1a3beb638633b
svnikitin <svnikitin@diasoft.ru>
2018-05-29 14:28:25 +0500

    remove trash

--- src/main/java/com/example/persistance/repository/OperationLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.persistance.repository;
-
-import com.example.persistance.entity.OperationLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "events", path = "events")
-public interface OperationLogRepository extends JpaRepository<OperationLog, Long> {
-}
--- src/main/java/com/example/web/controller/OperationGroupController.java
+++
@@ -1,4 +0,0 @@
-package com.example.web.controller;
-
-public class OperationGroupController {
-}
