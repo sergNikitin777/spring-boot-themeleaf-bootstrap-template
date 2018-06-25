b8338941d698e8743e4831f151851c8c3c277351
svnikitin <svnikitin@diasoft.ru>
2018-06-25 12:18:20 +0500

    refactoring app name

---
+++ src/main/java/com/example/EmercomApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmercomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmercomApplication.class, args);
+	}
+}
